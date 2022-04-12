c7a3bf8415d41c83b082df4c6c0f443ba8a59ef4
Diana Coelho <coelhodiana@MacBook-Pro-de-Diana.local>
2022-04-11 22:07:04 -0400

    feat: adiciona action sheet na página de reservas

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
+import { ActionSheetController, ModalController, NavController } from '@ionic/angular';
 
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 import { Place } from '../../place.model';
@@ -18,7 +18,8 @@ export class PlaceDetailPage implements OnInit {
     private navCtrl: NavController,
     private route: ActivatedRoute,
     private placesService: PlacesService,
-    private modal: ModalController
+    private modal: ModalController,
+    private actionSheet: ActionSheetController
   ) {}
 
   ngOnInit() {
@@ -33,6 +34,35 @@ export class PlaceDetailPage implements OnInit {
 
   onBookPlace() {
     //.this.navCtrl.navigateBack('/places/tabs/discover');
+
+    this.actionSheet.create({
+      header: 'Escolha uma ação',
+      buttons: [
+        {
+          text: 'Selecione a data',
+          handler: ()=>{
+            this.openBookingModal('select');
+          }
+        },
+        {
+          text: 'Data Aleatória',
+          handler: ()=>{
+            this.openBookingModal('random');
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    }).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+
+  }
+
+  openBookingModal(mode: 'select' | 'random') {
+    console.log(mode);
     this.modal
       .create({
         component: CreateBookingComponent,
